b311c559aff0d18f95f303a8f7ec9ef4b162d682
kevinherreroslechuga <kev.herreros@gmail.com>
2023-01-24 15:21:05 -0600

    Cargar challenge

---
+++ script.js
@@ -0,0 +1,70 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensajeEncriptado");
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+
+}
+
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringEncriptada = stringEncriptada.toLowerCase()
+    if(!mensajeValido(mensaje.value)){
+        alert("Ingresa solo texto y mas de 2 caracteres")
+    } else {
+        for(let i = 0; i < matrizCodigo.length; i++){
+            if(stringEncriptada.includes(matrizCodigo[i][0])){
+                stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+
+            }
+
+        }
+        alert("Mensaje encriptado con exito")
+        return stringEncriptada
+    }
+}
+
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+
+        }
+
+    }
+    alert("Mensaje desencriptado con exito")
+    return stringDesencriptada
+}
+
+
+function copiar(){
+    var copiarTexto = mensaje.value;
+    navigator.clipboard.writeText(copiarTexto);
+}
+
+function mensajeValido(mensaje){
+    let patron = /[a-zA-Z]{2,500}/;
+    return patron.test(mensaje);
+}
